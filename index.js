b5a3efca30d73444e5c0c51a0cf5d96c509aa70e
Muntazir Jahangir <muntazir9934@gmail.com>
2024-09-22 17:57:21 +0530

    user

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ const isValidBase64 = (str) => {
 
 app.post('/bfhl', upload.single('file'), (req, res) => {
     const { data, file_b64 } = req.body;
-    const userId = 'john_doe_17091999';
-    const email = 'john@xyz.com';
-    const rollNumber = 'ABCD123';
+    const userId = 'muntazirjahangir_13102002';
+    const email = 'muntazir9934@gmail.com';
+    const rollNumber = 'RA2111047010138';
 
     const numbers = [];
     const alphabets = [];
